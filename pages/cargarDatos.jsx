6c9bef90db4aad7240db23bf121f318ecc0df1c4
AleKurlat <79816198+AleKurlat@users.noreply.github.com>
2021-06-06 23:58:48 -0300

    aclaraciones

--- pages/cargarDatos.jsx
+++ pages/cargarDatos.jsx
@@ -11,16 +11,21 @@ export default function CargarDatos(props) {
     const { datosHeader, setDatosHeader, urlImagen, setUrlImagen, datosLateral, setDatosLateral, datosPrincipal, setDatosPrincipal, objPrincipalVacio, objLateralVacio, itemPrincipalVacio } = props;
 
     function generarCV() {
-        console.log(datosHeader);
-        console.log(urlImagen);
-        console.log(datosLateral);
-        console.log(datosPrincipal);
         router.push("/");
     }
 
     return (
         <div className="pagForm">
             <div className="contenedor">
+                <div className="alerta">
+                    <strong>
+                        <p>   Aclaración importante: en la versión actual este sitio NO conserva los datos ingresados, por lo cual se recomienda escribir el CV en un archivo de texto aparte y aquí solamente copiarlos. Si se refresca la ventana o se cierra, los datos se perderán.</p>
+                    </strong>
+                    <p>Una vez que se hace click en "generar CV", se pasa a otra pantalla donde se muestran los datos ingresados ya listos para imprimir (haciendo click en la opción "imprimir" del navegador). Para volver atrás y editar los datos, clickear en el botón "retroceder" del navegador.</p>
+
+                    App web programada en React / Next.JS, junio 2021.
+                    <a href="https://github.com/AleKurlat" style={{ "text-decoration": "none" }}>https://github.com/AleKurlat</a>
+                </div>
                 <FormHeader datosHeader={datosHeader} setDatosHeader={setDatosHeader} />
                 <div className="cuerpo">
                     <div className="barra-lateral">
