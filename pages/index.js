a653ea1b389288495cd7eca63f184c9f9408aebf
AleKurlat <79816198+AleKurlat@users.noreply.github.com>
2021-06-11 20:06:25 -0300

    renombre paginas

--- pages/index.js
+++ pages/index.js
@@ -1,54 +1,91 @@
-import Head from 'next/head';
-import Header from "../componentes/header";
-import Lateral from "../componentes/lateral";
-import Principal from "../componentes/principal";
+import FormHeader from "../componentes/formHeader";
+import FormLateral from "../componentes/formLateral";
+import FormPrincipal from "../componentes/formPrincipal";
+import FormImagen from "../componentes/formImagen";
+import React, { useState, useEffect } from 'react';
+import { Button } from 'reactstrap';
+import { useRouter } from 'next/router';
 import axios from "axios";
 import { preLoader, responderError, hostAPI, obtenerDatosToken } from "../librerias/lib.jsx";
-import React, { useState, useEffect } from 'react';
+import Link from 'next/link';
+import Layout from '../componentes/layout.jsx';
 
 export default function Home(props) {
-  //const { datosHeader, urlImagen, datosLateral, datosPrincipal } = props;
-  const { token } = props;
-  const autorizacion = { headers: { Authorization: token } };
-  const [datosCV, setDatosCV] = useState();
+    const router = useRouter();
+    const headerVacio = { nombre: "", descripcion: "" };
+    const objLateralVacio = { nombreItem: "", valorItem: "" };
+    const itemPrincipalVacio = { encabezadoP: "", parrafo: "" };
+    const objPrincipalVacio = { titulo: "", subtitulo: "", items: [itemPrincipalVacio] };
+    const lateralVacio = [objLateralVacio];
+    const principalVacio = [objPrincipalVacio];
+    const [datosHeader, setDatosHeader] = useState(headerVacio);
+    const [urlImagen, setUrlImagen] = useState("");
+    const [datosLateral, setDatosLateral] = useState(lateralVacio);
+    const [datosPrincipal, setDatosPrincipal] = useState(principalVacio);
+    const { token } = props;
+    const autorizacion = { headers: { Authorization: token } };
 
-  async function traerCV() {
-    try {
-      const datosToken = obtenerDatosToken(token);
-      const id = datosToken.user_id;
-      const urlAPI = hostAPI + "/cvs/" + id;
-      const loguear = await axios.get(urlAPI, autorizacion);
-      if (loguear && loguear.status === 200) {
-        setDatosCV(loguear.data);
-      }
+    async function traerCV() {
+        try {
+            const datosToken = obtenerDatosToken(token);
+            const id = datosToken.user_id;
+            const urlAPI = hostAPI + "/cvs/" + id;
+            const loguear = await axios.get(urlAPI, autorizacion);
+            if (loguear && loguear.status === 200) {
+                if (loguear.data) {
+                    setDatosHeader(loguear.data.datosHeader);
+                    setDatosLateral(loguear.data.datosLateral);
+                    setDatosPrincipal(loguear.data.datosPrincipal);
+                    setUrlImagen(loguear.data.urlImagen);
+                }
+            }
+        }
+        catch (e) {
+            return responderError(e)
+        }
     }
-    catch (e) {
-      return responderError(e)
+
+    async function guardarCV() {
+        try {
+            const datosToken = obtenerDatosToken(token);
+            const id = datosToken.user_id;
+            const urlAPI = hostAPI + "/cvs/" + id;
+            const objeto = { "cv": { datosHeader, urlImagen, datosLateral, datosPrincipal } }
+            const loguear = await axios.put(urlAPI, objeto, autorizacion);
+            if (loguear && loguear.status === 200) {
+                router.push("/verCV");
+            }
+        }
+        catch (e) {
+            return responderError(e)
+        }
     }
-  }
 
-  useEffect(() => {
-    if (token) { traerCV() }
-  }, [token]);
+    useEffect(() => {
+        if (token) { traerCV() }
+    }, [token]);
 
-  if (datosCV) {
-    const { datosHeader, datosLateral, datosPrincipal, urlImagen } = datosCV;
-    return (
-      <div className="contenedor">
-        <Head>
-          <title>Creá tu CV</title>
-          <meta name="description" content="App para generar tu Curriculum Vitae" />
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-        <Header datosHeader={datosHeader} />
-        <div className="cuerpo">
-          <Lateral datosLateral={datosLateral} urlImagen={urlImagen} />
-          <Principal datosPrincipal={datosPrincipal} />
-        </div>
-      </div>
-    )
-  } else {
-    return ("esperando datos");
-  }
-
+    if (token) {
+        return (
+            <Layout {...props}>
+                <div className="pagForm">
+                    <div className="contenedor">
+                        <FormHeader datosHeader={datosHeader} setDatosHeader={setDatosHeader} />
+                        <div className="cuerpo">
+                            <div className="barra-lateral">
+                                <FormImagen urlImagen={urlImagen} setUrlImagen={setUrlImagen} />
+                                <FormLateral datosLateral={datosLateral} setDatosLateral={setDatosLateral} objLateralVacio={objLateralVacio} />
+                            </div>
+                            <FormPrincipal datosPrincipal={datosPrincipal} setDatosPrincipal={setDatosPrincipal} objPrincipalVacio={objPrincipalVacio} itemPrincipalVacio={itemPrincipalVacio} />
+                        </div>
+                        <Button onClick={guardarCV} color="info" size="lg">Guardar datos y generar CV</Button>
+                    </div>
+                </div>
+            </Layout>
+        )
+    } else {
+        return (
+            <div>Por favor <Link href="/login">ingrese con su usuario</Link></div>
+        );
+    }
 }
---
+++ pages/verCV.jsx
@@ -0,0 +1,54 @@
+import Head from 'next/head';
+import Header from "../componentes/header";
+import Lateral from "../componentes/lateral";
+import Principal from "../componentes/principal";
+import axios from "axios";
+import { preLoader, responderError, hostAPI, obtenerDatosToken } from "../librerias/lib.jsx";
+import React, { useState, useEffect } from 'react';
+
+export default function VerCV(props) {
+  //const { datosHeader, urlImagen, datosLateral, datosPrincipal } = props;
+  const { token } = props;
+  const autorizacion = { headers: { Authorization: token } };
+  const [datosCV, setDatosCV] = useState();
+
+  async function traerCV() {
+    try {
+      const datosToken = obtenerDatosToken(token);
+      const id = datosToken.user_id;
+      const urlAPI = hostAPI + "/cvs/" + id;
+      const loguear = await axios.get(urlAPI, autorizacion);
+      if (loguear && loguear.status === 200) {
+        setDatosCV(loguear.data);
+      }
+    }
+    catch (e) {
+      return responderError(e)
+    }
+  }
+
+  useEffect(() => {
+    if (token) { traerCV() }
+  }, [token]);
+
+  if (datosCV) {
+    const { datosHeader, datosLateral, datosPrincipal, urlImagen } = datosCV;
+    return (
+      <div className="contenedor">
+        <Head>
+          <title>Creá tu CV</title>
+          <meta name="description" content="App para generar tu Curriculum Vitae" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <Header datosHeader={datosHeader} />
+        <div className="cuerpo">
+          <Lateral datosLateral={datosLateral} urlImagen={urlImagen} />
+          <Principal datosPrincipal={datosPrincipal} />
+        </div>
+      </div>
+    )
+  } else {
+    return ("esperando datos");
+  }
+
+}
