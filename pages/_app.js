1b162e17d3bc7135cfc0fa7a85c737cca8b5259a
AleKurlat <79816198+AleKurlat@users.noreply.github.com>
2021-06-11 21:28:53 -0300

    social meta tags

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@
 import '../styles/style.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { useState, useEffect } from 'react';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
   const [token, setToken] = useState();
@@ -19,7 +20,23 @@ function MyApp({ Component, pageProps }) {
 
   useEffect(chequearToken, []);
 
-  return <Component {...pageProps} {...pasarProps} />
+  return (
+    <>
+      <Head>
+        <title>Creá tu CV</title>
+        <meta name="description" content="App para generar tu Curriculum Vitae online" />
+        <meta property="og:site_name" content="Creá tu CV" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Creá tu CV" />
+        <meta property="og:description" content="App para generar tu Curriculum Vitae online" />
+        <meta property="og:image" content="https://image.flaticon.com/icons/png/512/760/760780.png" />
+        <meta name="twitter:image" content="https://image.flaticon.com/icons/png/512/760/760780.png" />
+        <meta name="twitter:title" content="Creá tu CV" />
+        <meta name="twitter:description" content="App para generar tu Curriculum Vitae online" />
+      </Head>
+      <Component {...pageProps} {...pasarProps} />
+    </>
+  )
 }
 
 export default MyApp
