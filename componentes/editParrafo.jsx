0fea41b8ba22a3ec1a4f3560f7f4db8b7faddf36
AleKurlat <79816198+AleKurlat@users.noreply.github.com>
2021-07-07 21:38:21 -0300

    desagregué parrafo

---
+++ componentes/editParrafo.jsx
@@ -0,0 +1,86 @@
+import { Form, FormGroup, Input, Button, Label, UncontrolledTooltip } from 'reactstrap';
+
+export default function editParrafo(props) {
+    const { item, j, i, datosPrincipal, refs, setDatosPrincipal } = props;
+
+    function handlerItem(evento, i, j) {
+        let arrayProvisorio = [...datosPrincipal];
+        arrayProvisorio[i].items[j] = { ...arrayProvisorio[i].items[j], [evento.target.name]: evento.target.value }
+        setDatosPrincipal(arrayProvisorio);
+    };
+
+    async function eliminarParrafo(i, j) {
+        if (datosPrincipal[i].items.length > 1) {
+            const confirmar = await swal({
+                text: "Estás por eliminar el párrafo seleccionado",
+                icon: "warning",
+                buttons: ["Cancelar", "Eliminar"],
+            });
+            if (confirmar) {
+                const elem = refs.current[i].current.children[0].children[1].children[j];
+                function callback() {
+                    elem.removeEventListener('transitionend', callback);
+                    let arrayProvisorio = [...datosPrincipal];
+                    arrayProvisorio[i].items = arrayProvisorio[i].items.filter((el, h) => { return (h != j) });
+                    setDatosPrincipal(arrayProvisorio);
+                }
+                elem.addEventListener('transitionend', callback);
+                elem.style.opacity = 0;
+            }
+        } else {
+            swal("Cada apartado debe tener al menos un párrafo");
+        }
+    }
+
+    function subirParrafo(numeroApartado, viejoOrdenParr) {
+        let arrayProvisorio = [...datosPrincipal];
+        let elementoMovido = arrayProvisorio[numeroApartado].items[viejoOrdenParr];
+        arrayProvisorio[numeroApartado].items = arrayProvisorio[numeroApartado].items.filter((elemento, j) => {
+            return (j !== viejoOrdenParr)
+        });
+
+        let nuevoOrdenParr = 0;
+        if (viejoOrdenParr > 0) { nuevoOrdenParr = viejoOrdenParr - 1 } else {
+            swal("Este párrafo ya es el primero de todos")
+        }
+        arrayProvisorio[numeroApartado].items.splice(nuevoOrdenParr, 0, elementoMovido);
+        setDatosPrincipal(arrayProvisorio);
+    }
+
+    function bajarParrafo(numeroApartado, viejoOrdenParr) {
+        const cantParrafos = datosPrincipal[numeroApartado].items.length;
+        let arrayProvisorio = [...datosPrincipal];
+        let elementoMovido = arrayProvisorio[numeroApartado].items[viejoOrdenParr];
+        arrayProvisorio[numeroApartado].items = arrayProvisorio[numeroApartado].items.filter((elemento, j) => {
+            return (j !== viejoOrdenParr)
+        });
+
+        let nuevoOrdenParr = cantParrafos - 1;
+        if (viejoOrdenParr < (cantParrafos - 1)) { nuevoOrdenParr = viejoOrdenParr + 1 } else {
+            swal("Este párrafo ya es el último de todos")
+        }
+        arrayProvisorio[numeroApartado].items.splice(nuevoOrdenParr, 0, elementoMovido);
+        setDatosPrincipal(arrayProvisorio);
+    }
+
+    return (
+        <div className="parrafo">
+            <FormGroup>
+                <Label><h3>Título del párrafo (optativo)</h3></Label>
+                <Input type="text" value={item.encabezadoP} name="encabezadoP" onChange={(evento) => { handlerItem(evento, i, j) }} placeholder="Escriba aquí (ejemplo: 'Atención al cliente')"></Input>
+            </FormGroup>
+            <FormGroup>
+                <Label>Párrafo</Label>
+                <Input type="textarea" rows="4" value={item.parrafo} name="parrafo" onChange={(evento) => { handlerItem(evento, i, j) }} placeholder="Escriba aquí (ejemplos: 'Empresa X', 'Período 2010-2015')"></Input>
+            </FormGroup>
+            <div className="botonera">
+                <Button color="primary" id={"subirParrafo" + i + "x" + j} onClick={() => { subirParrafo(i, j) }}> <img src="/arrowup.png" /></Button>
+                <UncontrolledTooltip placement="bottom" target={"subirParrafo" + i + "x" + j} >Reubicar párrafo hacia arriba</UncontrolledTooltip>
+                <Button color="primary" id={"bajarParrafo" + i + "x" + j} onClick={() => { bajarParrafo(i, j) }}> <img src="/arrowdown.png" /></Button>
+                <UncontrolledTooltip placement="bottom" target={"bajarParrafo" + i + "x" + j} >Reubicar párrafo hacia abajo</UncontrolledTooltip>
+                <Button color="primary" id={"eliminarParrafo" + i + "x" + j} onClick={() => { eliminarParrafo(i, j) }}><img src="/eliminar.svg" /></Button>
+                <UncontrolledTooltip placement="bottom" target={"eliminarParrafo" + i + "x" + j} >Eliminar párrafo</UncontrolledTooltip>
+            </div>
+        </div>
+    )
+}
--- componentes/formPrincipal.jsx
+++ componentes/formPrincipal.jsx
@@ -1,6 +1,7 @@
 import { Form, FormGroup, Input, Button, Label, UncontrolledTooltip } from 'reactstrap';
 import React, { useState, useEffect, useRef, createRef } from 'react';
 import swal from 'sweetalert';
+import EditParrafo from './editParrafo';
 
 export default function FormPrincipal(props) {
     const { datosPrincipal, setDatosPrincipal, objPrincipalVacio, itemPrincipalVacio } = props;
@@ -18,26 +19,8 @@ export default function FormPrincipal(props) {
                     </FormGroup>
                     <div className="parrafos">
                         {datosPrincipal[i].items.map((item, j) => {
-                            return (
-                                <div className="parrafo" key={i + "+" + j}>
-                                    <FormGroup>
-                                        <Label><h3>Título del párrafo (optativo)</h3></Label>
-                                        <Input type="text" value={item.encabezadoP} name="encabezadoP" onChange={(evento) => { handlerItem(evento, i, j) }} placeholder="Escriba aquí (ejemplo: 'Atención al cliente')"></Input>
-                                    </FormGroup>
-                                    <FormGroup>
-                                        <Label>Párrafo</Label>
-                                        <Input type="textarea" rows="4" value={item.parrafo} name="parrafo" onChange={(evento) => { handlerItem(evento, i, j) }} placeholder="Escriba aquí (ejemplos: 'Empresa X', 'Período 2010-2015')"></Input>
-                                    </FormGroup>
-                                    <div className="botonera">
-                                        <Button color="primary" id={"subirParrafo" + i + "x" + j} onClick={() => { subirParrafo(i, j) }}> <img src="/arrowup.png" /></Button>
-                                        <UncontrolledTooltip placement="bottom" target={"subirParrafo" + i + "x" + j} >Reubicar párrafo hacia arriba</UncontrolledTooltip>
-                                        <Button color="primary" id={"bajarParrafo" + i + "x" + j} onClick={() => { bajarParrafo(i, j) }}> <img src="/arrowdown.png" /></Button>
-                                        <UncontrolledTooltip placement="bottom" target={"bajarParrafo" + i + "x" + j} >Reubicar párrafo hacia abajo</UncontrolledTooltip>
-                                        <Button color="primary" id={"eliminarParrafo" + i + "x" + j} onClick={() => { eliminarParrafo(i, j) }}><img src="/eliminar.svg" /></Button>
-                                        <UncontrolledTooltip placement="bottom" target={"eliminarParrafo" + i + "x" + j} >Eliminar párrafo</UncontrolledTooltip>
-                                    </div>
-                                </div>
-                            )
+                            const objProps = { item, j, i, datosPrincipal, refs, setDatosPrincipal }
+                            return <EditParrafo {...objProps} key={i + "+" + j} />
                         })}
                     </div>
                     <div className="botonera">
@@ -60,12 +43,6 @@ export default function FormPrincipal(props) {
     function handler(evento, i) {
         let arrayProvisorio = [...datosPrincipal];
         arrayProvisorio[i] = { ...arrayProvisorio[i], [evento.target.name]: evento.target.value }
-        setDatosPrincipal(arrayProvisorio);
-    };
-
-    function handlerItem(evento, i, j) {
-        let arrayProvisorio = [...datosPrincipal];
-        arrayProvisorio[i].items[j] = { ...arrayProvisorio[i].items[j], [evento.target.name]: evento.target.value }
         setDatosPrincipal(arrayProvisorio);
     };
 
@@ -125,60 +102,6 @@ export default function FormPrincipal(props) {
         setDatosPrincipal(arrayProvisorio);
     }
 
-    async function eliminarParrafo(i, j) {
-        if (datosPrincipal[i].items.length > 1) {
-            const confirmar = await swal({
-                text: "Estás por eliminar el párrafo seleccionado",
-                icon: "warning",
-                buttons: ["Cancelar", "Eliminar"],
-            });
-            if (confirmar) {
-                const elem = refs.current[i].current.children[0].children[1].children[j];
-                console.log(elem)
-                function callback() {
-                    elem.removeEventListener('transitionend', callback);
-                    let arrayProvisorio = [...datosPrincipal];
-                    arrayProvisorio[i].items = arrayProvisorio[i].items.filter((el, h) => { return (h != j) });
-                    setDatosPrincipal(arrayProvisorio);
-                }
-                elem.addEventListener('transitionend', callback);
-                elem.style.opacity = 0;
-            }
-        } else {
-            swal("Cada apartado debe tener al menos un párrafo");
-        }
-    }
-
-    function subirParrafo(numeroApartado, viejoOrdenParr) {
-        let arrayProvisorio = [...datosPrincipal];
-        let elementoMovido = arrayProvisorio[numeroApartado].items[viejoOrdenParr];
-        arrayProvisorio[numeroApartado].items = arrayProvisorio[numeroApartado].items.filter((elemento, j) => {
-            return (j !== viejoOrdenParr)
-        });
-
-        let nuevoOrdenParr = 0;
-        if (viejoOrdenParr > 0) { nuevoOrdenParr = viejoOrdenParr - 1 } else {
-            swal("Este párrafo ya es el primero de todos")
-        }
-        arrayProvisorio[numeroApartado].items.splice(nuevoOrdenParr, 0, elementoMovido);
-        setDatosPrincipal(arrayProvisorio);
-    }
-
-    function bajarParrafo(numeroApartado, viejoOrdenParr) {
-        const cantParrafos = datosPrincipal[numeroApartado].items.length;
-        let arrayProvisorio = [...datosPrincipal];
-        let elementoMovido = arrayProvisorio[numeroApartado].items[viejoOrdenParr];
-        arrayProvisorio[numeroApartado].items = arrayProvisorio[numeroApartado].items.filter((elemento, j) => {
-            return (j !== viejoOrdenParr)
-        });
-
-        let nuevoOrdenParr = cantParrafos - 1;
-        if (viejoOrdenParr < (cantParrafos - 1)) { nuevoOrdenParr = viejoOrdenParr + 1 } else {
-            swal("Este párrafo ya es el último de todos")
-        }
-        arrayProvisorio[numeroApartado].items.splice(nuevoOrdenParr, 0, elementoMovido);
-        setDatosPrincipal(arrayProvisorio);
-    }
 
     function agregarParrafo(i) {
         let arrayProvisorio = [...datosPrincipal];
